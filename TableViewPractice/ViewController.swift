708605b6fa2bb45f63165baa45f53c99b0b382d2
zhshakuali <zhansayashakuali200309@gmail.com>
2023-07-11 16:13:58 +0600

    Initial Commit

---
+++ TableViewPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewPractice
+//
+//  Created by Жансая Шакуали on 11.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
